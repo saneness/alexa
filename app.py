9f4796f4b17515d7d356893956e72d2d3fa12a13
saneness <saneness@protonmail.com>
2022-11-13 00:50:04 +0000

    implement production version

--- app.py
+++ app.py
@@ -40,11 +40,3 @@ def help():
 @ask.session_ended
 def session_ended():
     return "{}", 200
-
-
-if __name__ == '__main__':
-    if 'ASK_VERIFY_REQUESTS' in os.environ:
-        verify = str(os.environ.get('ASK_VERIFY_REQUESTS', '')).lower()
-        if verify == 'false':
-            app.config['ASK_VERIFY_REQUESTS'] = False
-    app.run(debug=True)
